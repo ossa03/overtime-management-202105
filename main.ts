aaff6d9d31a31835f9a2848fcd22ef2914857993
ossa03 <kfcxd953pelo@gmail.com>
2021-11-20 23:42:34 +0900

    add total_time_cell

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 // スプレッドシート 時間外勤務表の管理
-const ssID = '1EhXDA7gHDe7hNZqbjDhERZabgYCrSsvIXaL-ohRr2Eg'
-const ss: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.openById(ssID)
+const ssId = '1EhXDA7gHDe7hNZqbjDhERZabgYCrSsvIXaL-ohRr2Eg'
+const ss: GoogleAppsScript.Spreadsheet.Spreadsheet = SpreadsheetApp.openById(ssId)
 const sheet: GoogleAppsScript.Spreadsheet.Sheet | null = ss.getSheetByName('今月')
+const sheetId = ss.getActiveSheet().getSheetId()
 const scriptProperties: GoogleAppsScript.Properties.Properties = PropertiesService.getScriptProperties()
 
 // トリガーで毎日AM6:00に実行する
@@ -18,8 +19,15 @@ function main() {
 	const pdfFile = createPdfFile_(pdfBlob)
 	const fileUrl = getFileUrl_(pdfFile)
 
+	/* Version2
+	   Urlfetchを使用したバージョン
+	 */
+	const { pdfBlob_ver2, file_ver2 } = createPdf_ver2_(FOLDER_ID, ssId, sheetId, fileName)
+	const fileUrl_ver2 = getFileUrl_(file_ver2)
+
 	// メールに送信
 	sendEmail_(pdfBlob, fileName, fileUrl)
+	sendEmail_(pdfBlob_ver2, fileName, fileUrl_ver2)
 
 	// LINEに送信
 	const message = `\n\n今月の時間外勤務表を送信しました．\n\n${fileName}\n${fileUrl}`
@@ -45,7 +53,7 @@ function sendLineNotify(message: string = createFileName_()) {
 
 // POSTリクエストが来たときの処理 ---------------------------------------------------
 function doPost(e: GoogleAppsScript.Events.DoPost) {
-	// ReactAppからPOSTされたデータを取得する
+	// NextAppからPOSTされたデータを取得する
 	const data: OvertimePostData = JSON.parse(e.postData.contents).data
 	const { radiologist, modality, date, start, end, description } = data
 
@@ -66,15 +74,21 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
 
 	// 10列目に残業時間（分）のヘッダーが無ければ入力する．
 	const Header_10 = '残業時間（分）'
-	const total_time_cell = sheet?.getRange(1, 10)
-	if (total_time_cell?.getValue() !== Header_10) {
-		total_time_cell?.setValue(Header_10)
+	const diff_time_cell = sheet?.getRange(1, 10)
+	if (diff_time_cell?.getValue() !== Header_10) {
+		diff_time_cell?.setValue(Header_10)
 	}
 	// 11列目に残業時間（時間）のヘッダーが無ければ 入力する．
 	const Header_11 = '残業時間（時間）'
-	const total_time_cell_2 = sheet?.getRange(1, 11)
-	if (total_time_cell_2?.getValue() !== Header_11) {
-		total_time_cell_2?.setValue(Header_11)
+	const diff_time_cell_2 = sheet?.getRange(1, 11)
+	if (diff_time_cell_2?.getValue() !== Header_11) {
+		diff_time_cell_2?.setValue(Header_11)
+	}
+	// 12列目に合計残業時間（時間(分)）のヘッダーが無ければ 入力する．
+	const Header_12 = '合計残業時間（時間(分)'
+	const total_time_cell = sheet?.getRange(1, 12)
+	if (total_time_cell?.getValue() !== Header_12) {
+		total_time_cell?.setValue(Header_12)
 	}
 
 	// spreadsheetに追加する．
@@ -92,6 +106,33 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
 		diff_time_2,
 	])
 
+	// 合計時間を計算
+	let total_time = 0
+	sheet
+		?.getDataRange()
+		.getValues()
+		.forEach((row) => {
+			const [
+				uuid,
+				created_at,
+				updated_at,
+				radiologist,
+				modality,
+				parsed_date,
+				start,
+				end,
+				description,
+				diff_time,
+				diff_time_2,
+			] = row
+
+			const _total = Number(diff_time) + Number(diff_time_2)
+			total_time = _total
+		})
+
+	// 12行目2列目に合計時間を入力
+	sheet?.getRange(2, 12, 1, 1).setValue(total_time.toFixed())
+
 	// スプレッドシートに書き込まれるまで少し待機
 	Utilities.sleep(5 * 1000)
 
@@ -99,7 +140,7 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
 	const ssUrl = ss.getUrl()
 	const crr_sheetName = ss.getActiveSheet().getName()
 
-	const postMessage = `「${crr_sheetName}」へ時間外勤務を登録しました．\n\n実施日: ${date}\n実施者: ${radiologist}\nモダリティ: ${modality}\n時間: ${start} 〜 ${end}\n業務内容: ${description}\n\n${ssUrl}`
+	const postMessage = `「NextAppから${crr_sheetName}」へ時間外勤務を登録しました．\n\n実施日: ${date}\n実施者: ${radiologist}\nモダリティ: ${modality}\n時間: ${start} 〜 ${end}\n業務内容: ${description}\n\n${ssUrl}`
 	// LINEへ通知
 	sendLineNotify(postMessage)
 }
@@ -157,4 +198,30 @@ function setScriptProperty_() {
 	scriptProperties.setProperty('LINE_NOTIFY_API_TOKEN', 'ys9d5Voj5gYDXK2GuAbskcpzq77gt2XDs3vMiKwseQB')
 
 	console.log(scriptProperties.getProperty('LINE_NOTIFY_API_TOKEN') ?? '')
+}
+
+function test() {
+	console.log('This is Test Script !')
+
+	// シートをコピー
+	copySheet_()
+	// let fileName = createFileName_()
+	const fileName = '_' + Utilities.getUuid()
+	const pdfBlob = createPdfBlob_(ss, fileName)
+	const pdfFile = createPdfFile_(pdfBlob)
+	const fileUrl = getFileUrl_(pdfFile)
+
+	/* Version2
+	   Urlfetchを使用したバージョン
+	 */
+	const { pdfBlob_ver2, file_ver2 } = createPdf_ver2_(FOLDER_ID, ssId, sheetId, fileName)
+	const fileUrl_ver2 = getFileUrl_(file_ver2)
+
+	// メールに送信
+	sendEmail_(pdfBlob, fileName, fileUrl)
+	sendEmail_(pdfBlob_ver2, fileName, fileUrl_ver2)
+
+	// LINEに送信
+	const message = `\n\nTestです今月の時間外勤務表を送信しました．.\n\n${fileName}\n${fileUrl}`
+	sendLineNotify(message)
 }
--- utils.ts
+++ utils.ts
@@ -1,20 +1,20 @@
-// ファイル名を作成する．'202105'
+// ファイル名を作成する．'202105' スクリプト実行の前月を取得したい
 const createFileName_ = (): string => {
-	return Utilities.formatDate(new Date(), 'JST', 'yyyyMM')
+	const d = new Date()
+	const lastMonth = d.getMonth() - 1
+	d.setMonth(lastMonth)
+	return Utilities.formatDate(d, 'JST', 'yyyyMM')
 }
 
 // スプレッドシートをPDFとして取得する
-const createPdfBlob_ = (
-	spreadSheet: GoogleAppsScript.Spreadsheet.Spreadsheet,
-	fileName: string,
-): GoogleAppsScript.Base.Blob => {
-	const pdfBlob = spreadSheet.getAs('application/pdf').setName(`${fileName}.pdf`)
+const createPdfBlob_ = (ss: GoogleAppsScript.Spreadsheet.Spreadsheet, fileName: string): GoogleAppsScript.Base.Blob => {
+	const pdfBlob = ss.getAs('application/pdf').setName(`${fileName}.pdf`)
 	return pdfBlob
 }
 
 // googleDriveの指定のフォルダ("過去データ")へ保存する
+const FOLDER_ID = '1oVv95yEt3Pm1itm8ocqKW8ij88ZXTobG'
 const createPdfFile_ = (blob: GoogleAppsScript.Base.Blob): GoogleAppsScript.Drive.File => {
-	const FOLDER_ID = '1oVv95yEt3Pm1itm8ocqKW8ij88ZXTobG'
 	const folder = DriveApp.getFolderById(FOLDER_ID) //フォルダを指定
 	const pdfFile = folder.createFile(blob)
 	return pdfFile
@@ -41,4 +41,62 @@ const sendEmail_ = (pdfBlob: GoogleAppsScript.Base.Blob, fileName: string, fileU
 		`PDFを送りました\n` + fileUrl, // 本文
 		{ attachments: [pdfBlob] }, // 添付ファイル(pdf)
 	)
+}
+
+//PDFを作成し指定したフォルダーに保存する関数
+//以下4つの引数を指定する必要がある
+//1: フォルダーID (FOLDER_ID)
+//2: スプレッドシートID (sheetId)
+//3: シートID (shId)
+//4: ファイル名 (fileName)
+function createPdf_ver2_(folderId: string, ssId: string, sheetId: number, fileName: string) {
+	//PDFを作成するためのベースとなるURL
+	const baseUrl = 'https://docs.google.com/spreadsheets/d/' + ssId + '/export?gid=' + sheetId
+
+	//★★★自由にカスタマイズしてください★★★
+	//PDFのオプションを指定
+	const pdfOptions =
+		'&exportFormat=pdf&format=pdf' +
+		'&size=A4' + //用紙サイズ (A4)
+		'&portrait=true' + //用紙の向き true: 縦向き / false: 横向き
+		'&fitw=true' + //ページ幅を用紙にフィットさせるか true: フィットさせる / false: 原寸大
+		'&top_margin=0.50' + //上の余白
+		'&right_margin=0.50' + //右の余白
+		'&bottom_margin=0.50' + //下の余白
+		'&left_margin=0.50' + //左の余白
+		'&horizontal_alignment=CENTER' + //水平方向の位置
+		'&vertical_alignment=TOP' + //垂直方向の位置
+		'&printtitle=false' + //スプレッドシート名の表示有無
+		'&sheetnames=true' + //シート名の表示有無
+		'&gridlines=false' + //グリッドラインの表示有無
+		'&fzr=false' + //固定行の表示有無
+		'&fzc=false' //固定列の表示有無;
+
+	//PDFを作成するためのURL
+	const url = baseUrl + pdfOptions
+
+	//アクセストークンを取得する
+	const token = ScriptApp.getOAuthToken()
+
+	//headersにアクセストークンを格納する
+	const options = {
+		headers: {
+			Authorization: 'Bearer ' + token,
+		},
+		muteHttpExceptions: true,
+	}
+
+	//PDFを作成する
+	const pdfBlob_ver2 = UrlFetchApp.fetch(url, options)
+		.getBlob()
+		.setName(fileName + '_ver2' + '.pdf')
+
+	//PDFの保存先フォルダー
+	//フォルダーIDは引数のfolderIdを使用します
+	const folder = DriveApp.getFolderById(folderId)
+
+	//PDFを指定したフォルダに保存する
+	const file_ver2 = folder.createFile(pdfBlob_ver2)
+
+	return { pdfBlob_ver2, file_ver2 }
 }
